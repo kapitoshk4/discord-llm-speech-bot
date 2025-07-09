1f06b1f682a3bab77429c525db6c59a445685d51
Dmytro Shchoma <dmytroshchoma@gmail.com>
2025-07-09 22:45:48 +0300

    add loading command & events files in bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
-const { Client, IntentsBitField } = require("discord.js")
+const path = require("node:path");
+const fs = require("node:fs");
+const { Client, IntentsBitField, Events } = require("discord.js")
 
 const client = new Client({
   intents: [ 
@@ -13,7 +15,32 @@ const client = new Client({
 
 const TOKEN = process.env.DISCORD_TOKEN;
 
-client.on("ready", (с) => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ("data" in command && "execute" in command) {
+    if (!client.commands) client.commands = new Map();
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(`The command at ${filePath} is missing a required "data" or "execute" property.`);
+  }
+}
+
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 client.login(TOKEN);
