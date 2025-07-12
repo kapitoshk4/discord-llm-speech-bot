5d131200363f9c753ef2c07165f4b3fcc54c67aa
Dmytro Shchoma <dmytroshchoma@gmail.com>
2025-07-12 18:49:27 +0300

    update input dictionary in openApiRequest function

--- src/api/llm/openai_chat.js
+++ src/api/llm/openai_chat.js
@@ -1,11 +1,15 @@
 const { app } = require("../llm/langgraph_config.js");
 const { getThreadId } = require("../llm/memory.js");
 
-async function openaiApiRequest(userId, prompt) {
+async function openaiApiRequest(userId, prompt, imageUrl = null) {
   try {
     const threadId = getThreadId(userId);
     const config = { configurable: { thread_id: threadId } };
-    const result = await app.invoke({ input: prompt }, config);
+    const input = {
+      input: prompt,
+      imageUrl: imageUrl,
+    }
+    const result = await app.invoke(input, config);
     console.log(`LangChain response: ${result.answer}`);
     return result.answer;
   } catch (error) {
