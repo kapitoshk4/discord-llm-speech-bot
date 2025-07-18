3f2c38f3f20a6cf7788141840fd6d205300bdcf8
Dmytro Shchoma <dmytroshchoma@gmail.com>
2025-07-19 00:23:43 +0300

    create audio/sst_openai.js & write function to send audio to stt

---
+++ src/api/audio/sst_openai.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+const FormData = require("form-data");
+const { Readable } = require("stream");
+
+function bufferToStream(buffer) {
+    const stream = new Readable();
+    stream.push(buffer);
+    stream.push(null);
+    return stream;
+}
+
+async function sendAudioToAPI(buffer) {
+    const form = new FormData();
+
+    form.append("file", bufferToStream(buffer), {
+        filename: `audio.mp3`,
+        contentType: "audio/mpeg"
+    });
+
+    form.append("model", process.env.OPENAI_TRANSCRIPTION_MODEL);
+
+    try {
+        const response = await axios.post("https://api.openai.com/v1/audio/transcriptions", form, {
+            headers: {
+                Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+                ...form.getHeaders()
+            }
+        });
+        return response.data.text;
+
+    } catch (error) {
+        console.error("Error transcribing audio:", error?.response?.data || error.message);
+    }
+}
+
+
+
+module.exports = { sendAudioToAPI };
