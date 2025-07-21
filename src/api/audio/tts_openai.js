66497689b89244ebbd5d02d9beeabcd0c7ee542b
Dmytro Shchoma <dmytroshchoma@gmail.com>
2025-07-21 23:41:08 +0300

    change response format to wav in tts_openai.js

--- src/api/audio/tts_openai.js
+++ src/api/audio/tts_openai.js
@@ -5,17 +5,18 @@ const openai = new OpenAI({
 });
 
 async function sendTextToTTS(text) {
-    const opusAudioResponse = await openai.audio.speech.create({
+    const response = await openai.audio.speech.create({
         model: process.env.OPENAI_TTS_MODEL,
         voice: process.env.OPENAI_TTS_VOICE,
         input: text,
         instructions: process.env.OPENAI_TTS_INSTRUCTIONS,
-        response_format: "opus",
+        response_format: "wav",
     });
 
-    const buffer = Buffer.from(await opusAudioResponse.arrayBuffer());
-    console.log(`Generated TTS audio of length: ${buffer.length} bytes`);
-    return buffer;
+    // const buffer = Buffer.from(await opusAudioResponse.arrayBuffer());
+    // console.log(`Generated TTS audio of length: ${buffer.length} bytes`);
+    // return buffer;
+    return response.body;
 }
 
 module.exports = { sendTextToTTS };
