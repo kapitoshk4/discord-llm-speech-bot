38de62138d50e4412f64da2222d984297954ef3c
Dmytro Shchoma <dmytroshchoma@gmail.com>
2025-07-28 22:08:59 +0300

    add split function for bot names through env

--- src/services/transcription.js
+++ src/services/transcription.js
@@ -2,7 +2,7 @@ function handleTranscription(transcription, userName, requireMention) {
     if (!transcription || transcription.trim().split(/\s+/).length < 2) {
         return ["Skipping transcription, too short.", false];
     }
-    const botNames = ["bot", "бот", "timzee", "тимзи"];
+    const botNames = process.env.BOT_NAMES.split(',').map(name => name.trim().toLowerCase());
     resultWithoutPunctuationToLowerCase = transcription.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").toLowerCase();
     if (requireMention){
         const mentioned = botNames.some(name => resultWithoutPunctuationToLowerCase.includes(name));
