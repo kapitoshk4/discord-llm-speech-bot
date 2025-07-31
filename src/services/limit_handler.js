6f34f8e5c9fca6c5599a66f6b046802b248d0b45
Dmytro Shchoma <dmytroshchoma@gmail.com>
2025-07-31 23:34:45 +0300

    create limit_handler.js & add function to handle limits for free tier users

---
+++ src/services/limit_handler.js
@@ -0,0 +1,34 @@
+const { User, Permission, CommandUsage } = require("../db/models");
+const { Op } = require("@sequelize/core");
+
+async function handleLimit(userId) {
+    const user = await User.findOne({
+        where: { discordId: userId },
+        include: Permission
+    });
+    
+    if (user.Permission.name != "normal") {
+        return true;
+    }
+
+    const oneWeekAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)
+    
+    const usageCount = await CommandUsage.count({
+        where: {
+            userId: userId,
+            createdAt: { [Op.gte]: oneWeekAgo }
+        }
+    });
+
+    if (usageCount >= process.env.MAX_GENERATIONS_PER_WEEK) {
+        return false;
+    }
+
+    await CommandUsage.create({
+        userId: userId
+    });
+    
+    return true;
+}
+
+module.exports = { handleLimit }
